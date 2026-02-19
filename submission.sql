79593a3b88181ba69fcaf7229c0fb4d182dd67d7
Michael <michaelflorentino4444@gmail.com>
2026-02-19 10:05:49 -0500

    Mainframe override complete

---
+++ submission.sql
@@ -0,0 +1,21 @@
+-- Psql commands for Mainframe Override --
+
+Select * from forum_posts Where date Between '2048-04-01' and '2048-05-01'
+and content ilike '%emptystack%' and content ilike '%dad%';
+
+Select * from forum_accounts Where username = 'smart-money-44';
+
+Select * from forum_accounts Where last_name = 'Steele';
+
+Select * from emptystack_accounts Where last_name = 'Steele';
+
+--  triple-cart-38 | password456 | Andrew     | Steele --
+
+Select * From emptystack_messages Where body ilike '%project%';
+
+Select * From emptystack_accounts Where username = 'your-boss-99';
+
+-- LidWj | your-boss-99 | triple-cart-38 | Project TAXI | Deploy Project TAXI by end of week. We need this out ASAP. --
+--  your-boss-99 | notagaincarter | Skylar     | Singer --
+
+--  DczE0v2b | TAXI --
